230b095e2e0326619f78a3acb49466e1b51fbe25
Lars Kjeldgaard <lars_kjeldgaard@hotmail.com>
2020-07-06 15:03:02 +0200

    reduced example

--- app.py
+++ app.py
@@ -1,20 +1,33 @@
-from flask import Flask, jsonify, request
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    if (request.method == 'POST'):
-        some_json = request.get_json()
-        return jsonify({'you sent': some_json}), 201
-    else:
-        return jsonify({'about':'Hello World!'})
+@app.route('/')
+def home():
+    return 'heeeellloow world!'
 
-@app.route('/multi/<int:num>', methods=['GET'])
-def get_multiply10(num):
-    return jsonify({'result': num * 10})
+if __name__ == '__main__':
+    app.run()
 
-app.run(debug=True)
+
+
+# from flask import Flask, jsonify, request
+
+# app = Flask(__name__)
+
+# @app.route('/', methods=['GET', 'POST'])
+# def index():
+#     if (request.method == 'POST'):
+#         some_json = request.get_json()
+#         return jsonify({'you sent': some_json}), 201
+#     else:
+#         return jsonify({'about':'Hello World!'})
+
+# @app.route('/multi/<int:num>', methods=['GET'])
+# def get_multiply10(num):
+#     return jsonify({'result': num * 10})
+
+# app.run(debug=True)
 
 # curl http://127.0.0.1:5000/
 # curl http://127.0.0.1:5000/multi/10
