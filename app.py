fe08a0c837ae00392229ba485c42bad52bc70b96
Lars Kjeldgaard <lars_kjeldgaard@hotmail.com>
2020-07-06 15:13:04 +0200

    jsoN

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
@@ -6,11 +6,19 @@ app = Flask(__name__)
 def home():
     return 'heeeellloow world!'
 
-
 @app.route('/multi/<int:num>', methods=['GET'])
 def get_multiply10(num):
     return jsonify({'result': num * 10})
-    
+
+@app.route('/json/', methods=['GET', 'POST'])
+def index():
+    if (request.method == 'POST'):
+        some_json = request.get_json()
+        return jsonify({'you sent': some_json}), 201
+    else:
+        return jsonify({'about':'Hello World!'})
+
+
 if __name__ == '__main__':
     app.run()
 
@@ -20,18 +28,8 @@ if __name__ == '__main__':
 
 # app = Flask(__name__)
 
-# @app.route('/', methods=['GET', 'POST'])
-# def index():
-#     if (request.method == 'POST'):
-#         some_json = request.get_json()
-#         return jsonify({'you sent': some_json}), 201
-#     else:
-#         return jsonify({'about':'Hello World!'})
-
-
 # app.run(debug=True)
 
 # curl http://127.0.0.1:5000/
 # curl http://127.0.0.1:5000/multi/10
 # curl -H "Content-Type: application/json" -X POST -d "{\"name\":\"xyz\", \"address\":\"address xyz\"}" http://127.0.0.1:5000/
-
