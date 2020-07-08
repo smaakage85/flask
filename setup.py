6b0d5d8b595622eba7895567f251a825f6066f81
Lars Kjeldgaard <lars_kjeldgaard@hotmail.com>
2020-07-08 20:57:43 +0200

    try to please actions

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setuptools.setup(
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: Linux",
+        "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
     include_package_data=True
