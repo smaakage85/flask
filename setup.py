ba91b9dfa4389fb26db162891d4682e30bf024ba
Lars Kjeldgaard <lars_kjeldgaard@hotmail.com>
2020-07-08 20:40:52 +0200

    blah

---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="irismodel", 
+    version="0.0.1",
+    author="Lars Kjeldgaard",
+    author_email="lars_kjeldgaard@hotmail.com",
+    description="Model for Iris Dataset",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'sklearn',
+        'numpy'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Linux",
+    ],
+    python_requires='>=3.6',
+    include_package_data=True
+    )
